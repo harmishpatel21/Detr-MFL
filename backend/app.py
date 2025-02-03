55edb3956f9723eab409b6f939a20e52036ff490
Harmish Patel <hpatel@entegrasolutions.com>
2025-02-03 14:32:07 -0500

    Setup front-back communication

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,44 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from PIL import Image
 import io 
+import os 
+import base64
+
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.post("/upload/")
 async def upload_image(file: UploadFile = File(...)):
+    print("Here")
+    os.makedirs("images", exist_ok=True)
+
     contents = await file.read()
     img = Image.open(io.BytesIO(contents))
-    img.save(f"./images/original_{file.filename}")
+    original_path = f"./images/original_{file.filename}"
+    img.save(original_path)
 
-    img = img.convert("L")
-    img.save(f"./images/processed_{file.filename}.png")
+    byte_io = io.BytesIO()
 
-    return {"filename": file.filename, 
+    # processed_path = f"./images/processed_{file.filename}"
+    processed_img = img.convert("L")
+    processed_img.save(byte_io, format='PNG')
+    byte_io.seek(0)
+    
+    processed_path = f"./images/processed_{file.filename}"   
+    img.save(processed_path)
+
+    return {"filename": file.filename,
+            "processed_image": base64.b64encode(byte_io.getvalue()).decode('utf-8'),
             "message": "Image Processing success"}
 
 @app.get("/", response_class=HTMLResponse)
